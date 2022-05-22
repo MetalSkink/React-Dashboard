259df463f0848e968a2baa7bea1d0db6ef141031
Alucard <xavier_ortega98@hotmail.com>
2022-05-22 01:53:19 -0500

    Navbar and Sidenav finished

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import './App.css';
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorPicker, ColorMapping, Editor} from "./pages";
+import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   useEffect(() => {
     
   }, [])
